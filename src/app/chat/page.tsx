70f5649a1d03916debcc377444510ba98aea76b9
938938 <938kjh@gmail.com>
2025-04-23 23:27:02 +0900

    [FEAT] 연결

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,3 +1,6 @@
+import ChatPeopleList from "@/components/chat/ChatPeopleList";
+import ChatScreen from "@/components/chat/ChatScreen";
+
 const ChatPage = () => {
   return (
     <div className='w-full h-screen flex justify-center items-center'>

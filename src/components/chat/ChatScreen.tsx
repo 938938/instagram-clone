ebf72babb5c4e15d83390e20eeea7e707a3870a5
938938 <938kjh@gmail.com>
2025-05-09 23:25:37 +0900

    [FEAT] 채팅기능 구현

--- src/components/chat/ChatScreen.tsx
+++ src/components/chat/ChatScreen.tsx
@@ -13,19 +13,23 @@ import {
   sendMessage,
 } from '@/actions/chatActions';
 import { useMutation, useQuery } from '@tanstack/react-query';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Spinner } from '@material-tailwind/react';
+import { createBrowserSupabaseClient } from '@/utils/supabase/client';
 
 const ChatScreen = () => {
   const selectedUserId = useRecoilValue(selectedUserIdState);
   const selectedUserIndex = useRecoilValue(selectedUserIndexState);
   const [message, setMessage] = useState<string>('');
 
+  const supabase = createBrowserSupabaseClient();
+
   const selectedUserQuery = useQuery({
     queryKey: ['user', selectedUserId],
     queryFn: () => getUserById(selectedUserId),
   });
 
-  const sendMessageMutaton = useMutation({
+  const sendMessageMutation = useMutation({
     mutationFn: async () => {
       return sendMessage({
         message,
@@ -43,6 +47,31 @@ const ChatScreen = () => {
     queryFn: () => getAllMessages({ chatUserId: selectedUserId }),
   });
 
+  useEffect(() => {
+    const channel = supabase
+      .channel('message_postgres_changes')
+      .on(
+        'postgres_changes',
+        {
+          event: 'INSERT',
+          schema: 'public',
+          table: 'message',
+        },
+        (payload) => {
+          if (payload.eventType === 'INSERT' && !payload.errors) {
+            getAllMessagesQuery.refetch();
+          }
+        }
+      )
+      .subscribe();
+
+    return () => {
+      channel.unsubscribe();
+    };
+  }, []);
+
+  console.log(getAllMessagesQuery.data);
+
   return selectedUserQuery.data !== null ? (
     <div className='w-full h-screen flex flex-col'>
       <Person
@@ -54,26 +83,30 @@ const ChatScreen = () => {
         userId={selectedUserQuery.data?.id}
       />
 
-      <div className='w-full flex-1 flex flex-col p-4 gap-3'>
-        <Message isFromMe={true} message={'안녕하세요.'} />
-        <Message isFromMe={false} message={'반갑습니다.'} />
-        <Message isFromMe={true} message={'안녕하세요.'} />
-        <Message isFromMe={true} message={'안녕하세요.'} />
-        <Message isFromMe={false} message={'반갑습니다.'} />
-        <Message isFromMe={false} message={'반갑습니다.'} />
+      <div className='w-full overflow-y-scroll flex-1 flex flex-col p-4 gap-3'>
+        {getAllMessagesQuery.data?.map((message) => (
+          <Message
+            key={message.id}
+            message={message.message}
+            isFromMe={message.receiver === selectedUserId}
+          />
+        ))}
       </div>
 
       <div className='flex'>
         <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
           className='p-3 w-full border-2 border-light-blue-600'
           placeholder='메시지를 입력하세요.'
         />
 
         <button
+          onClick={() => sendMessageMutation.mutate()}
           className='min-w-20 p-3 bg-light-blue-600 text-white'
           color='light-blue'
         >
-          <span>전송</span>
+          {sendMessageMutation.isPending ? <Spinner /> : <span>전송</span>}
         </button>
       </div>
     </div>
--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -4,9 +4,7 @@ const Message = ({ isFromMe, message }) => {
   return (
     <div
       className={`w-fit p-3 rounded-md ${
-        isFromMe
-          ? 'ml-auto bg-light-blue-600 text-white'
-          : 'bg-gray-100 text-black'
+        isFromMe ? 'ml-auto bg-blue-400 text-white' : 'bg-gray-100 text-black'
       }`}
     >
       <p>{message}</p>
